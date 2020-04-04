1d7bccf160ba5ad6d034df15a3de637e8a273d5b
Damien Maillard <damien.maillard@dailymotion.com>
2020-04-04 14:16:54 +0200

    push somthing for merge conflict

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { generateSnapshotFile } from "./src/generateSnapshotFile.js"
-export { jsenvDirectorySizeTrackingConfig } from "./src/jsenvDirectorySizeTrackingConfig.js"
+export { senvDirectorySizeTrackingConfig } from "./src/jsenvDirectorySizeTrackingConfig.js"
 export { reportSizeImpactIntoGithubPullRequest } from "./src/reportSizeImpactIntoGithubPullRequest.js"
