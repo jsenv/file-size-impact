8bf36287e597f2f6de883f6cf7069b90e4c8bf92
Damien Maillard <damien.maillard@dailymotion.com>
2020-04-05 08:16:11 +0200

    update size impact workflow

--- .github/workflows/size-impact/report-size-impact.js
+++ .github/workflows/size-impact/report-size-impact.js
@@ -1,8 +1,9 @@
 import { reportSizeImpactIntoGithubPullRequest } from "@jsenv/github-pull-request-filesize-impact"
+import { projectDirectoryUrl } from "../../../jsenv.config.js"
 
 reportSizeImpactIntoGithubPullRequest({
   logLevel: "debug",
-  projectDirectoryUrl: new URL("../../", import.meta.url),
+  projectDirectoryUrl,
   baseSnapshotFileRelativeUrl: process.argv[2],
   headSnapshotFileRelativeUrl: process.argv[3],
   generatedByLink: false,
