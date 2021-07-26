375c6e4e04d0c668bb8ec71d0b514c6b05915766
Damien Maillard <damien.maillard@dailymotion.com>
2021-07-26 15:55:54 +0200

    rename workflow

--- .github/workflows/file_size_impact/report_file_size_impact.js
+++ .github/workflows/file_size_impact/report_file_size_impact.js
@@ -5,9 +5,9 @@ reportFileSizeImpact({
   logLevel: "debug",
   buildCommand: "npm run dist",
   trackingConfig: {
-    "dist/commonjs": {
-      "./dist/commonjs/**/*": true,
-      "./dist/commonjs/**/*.map": false,
+    "some files": {
+      "./index.js": true,
+      "./src/reportFileSizeImpact.js": true,
     },
   },
   transformations: { raw, gzip, brotli },
